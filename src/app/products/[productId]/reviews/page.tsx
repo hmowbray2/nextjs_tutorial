3938bea09d2b87ccdc260150c513cc4834458ac3
Hannah Mowbray <hannahmowbray3@gmail.com>
2025-04-02 18:46:25 +0100

    catch all segments
    Signed-off-by: Hannah Mowbray <hannahmowbray3@gmail.com>

--- src/app/products/[productId]/reviews/page.tsx
+++ src/app/products/[productId]/reviews/page.tsx
@@ -1,3 +1,15 @@
-export default function Review() {
-  return <h1>Reviews</h1>;
+export default async function Review({
+  params,
+}: {
+  params: Promise<{ productId: string }>;
+}) {
+  const { productId } = await params;
+  return (
+    <>
+      <h1>Reviews for product {productId}</h1>
+      <h2>Review 1</h2>
+      <h2>Review 2</h2>
+      <h2>Review 3</h2>
+    </>
+  );
 }
